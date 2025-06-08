d9b3a95c7876ac07369014b1ea3b2069d189a0f8
Dajesa <93830906+Dajesa@users.noreply.github.com>
2025-06-08 09:23:59 -0500

    Cambios Archivos Estructuras

--- src/edu_pad/main_extractor.py
+++ src/edu_pad/main_extractor.py
@@ -1,19 +1,16 @@
-from edu_pad.dataweb import DataWeb
+from src.edu_pad.dataweb import DataWeb
 import pandas as pd
-import os
+
+
 
 def main():
     dataweb = DataWeb()
-    list_indicadores = ["DOLA-USD", "EURUSD%3DX", "CL%3DF", "GC%3DF"]
-
-    # Crear carpeta si no existe
-    output_dir = "src/edu_pad/static/csv"
-    os.makedirs(output_dir, exist_ok=True)
-
+    list_indicadores = ["DOLA-USD","EURUSD%3DX","CL%3DF","GC%3DF"]
     for indicador in list_indicadores:
         df = dataweb.procesar_indicador_completo(indicador)
-        output_path = os.path.join(output_dir, "data_extractor.csv")
-        df.to_csv(output_path, index=False, mode="a")
+        df.to_csv("src/edu_pad/static/csv/data_extractor.csv", index=False,mode="a")
+
+
 
 if __name__ == "__main__":
     main()
--- src/edu_pad/main_ingesta.py
+++ src/edu_pad/main_ingesta.py
@@ -1,5 +1,5 @@
 
-from edu_pad.database import DataBase
+from src.edu_pad.database import DataBase
 import pandas as pd
 
 
@@ -7,9 +7,7 @@ import pandas as pd
 def main():
     database = DataBase()
     df = pd.read_csv("src/edu_pad/static/csv/data_extractor.csv")
-    
     df_db = database.guardar_df(df)
-    
     #df_db2 = database.obtener_datos() # capa 3 guardar 
     #df_db2.to_csv("static/csv/data_db.csv", index=False)
 
