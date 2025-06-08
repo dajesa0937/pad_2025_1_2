09079ffbfa3c3aa9fa65e0cd7ef78736492de37d
Dajesa <93830906+Dajesa@users.noreply.github.com>
2025-06-08 09:36:21 -0500

    Modificar Archivo Extractore Junio 08 2025

--- src/edu_pad/main_extractor.py
+++ src/edu_pad/main_extractor.py
@@ -1,17 +1,16 @@
-import os
-from edu_pad.dataweb  import DataWeb
+from src.edu_pad.dataweb import DataWeb
 import pandas as pd
 
-def main():
-    # Crear la ruta si no existe
-    output_dir = "src/edu_pad/static/csv"
-    os.makedirs(output_dir, exist_ok=True)
 
+
+def main():
     dataweb = DataWeb()
-    list_indicadores = ["DOLA-USD", "EURUSD%3DX", "CL%3DF", "GC%3DF"]
+    list_indicadores = ["DOLA-USD","EURUSD%3DX","CL%3DF","GC%3DF"]
     for indicador in list_indicadores:
         df = dataweb.procesar_indicador_completo(indicador)
-        df.to_csv(os.path.join(output_dir, "data_extractor.csv"), index=False, mode="a")
+        df.to_csv("src/edu_pad/static/csv/data_extractor.csv", index=False,mode="a")
+
+
 
 if __name__ == "__main__":
     main()
