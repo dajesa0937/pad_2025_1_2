563b106f746d35fc469256762713d23237105702
Dajesa <93830906+Dajesa@users.noreply.github.com>
2025-06-08 09:26:48 -0500

    Modificacion de Archivos

--- src/edu_pad/main_ingesta.py
+++ src/edu_pad/main_ingesta.py
@@ -7,7 +7,8 @@ import pandas as pd
 def main():
     database = DataBase()
     df = pd.read_csv("src/edu_pad/static/csv/data_extractor.csv")
-    df_db = database.guardar_df(df)
+    database.guardar_df(df)
+    
     #df_db2 = database.obtener_datos() # capa 3 guardar 
     #df_db2.to_csv("static/csv/data_db.csv", index=False)
 
