e7362f8bb4a246c537bafbc1504cb17bffade7f7
Dajesa <93830906+Dajesa@users.noreply.github.com>
2025-06-08 09:08:25 -0500

    Actualizacion de archivo extractor

--- src/edu_pad/main_extractor.py
+++ src/edu_pad/main_extractor.py
@@ -1,16 +1,19 @@
-from edu_pad.dataweb  import DataWeb
+from edu_pad.dataweb import DataWeb
 import pandas as pd
-
-
+import os
 
 def main():
     dataweb = DataWeb()
-    list_indicadores = ["DOLA-USD","EURUSD%3DX","CL%3DF","GC%3DF"]
+    list_indicadores = ["DOLA-USD", "EURUSD%3DX", "CL%3DF", "GC%3DF"]
+
+    # Crear carpeta si no existe
+    output_dir = "src/edu_pad/static/csv"
+    os.makedirs(output_dir, exist_ok=True)
+
     for indicador in list_indicadores:
         df = dataweb.procesar_indicador_completo(indicador)
-        df.to_csv("src/edu_pad/static/csv/data_extractor.csv", index=False,mode="a")
-
-
+        output_path = os.path.join(output_dir, "data_extractor.csv")
+        df.to_csv(output_path, index=False, mode="a")
 
 if __name__ == "__main__":
     main()
